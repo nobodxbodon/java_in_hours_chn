77c62e540687a69d5a8bf96f9e48590988f3bd4f
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-07-08 00:21:33 -0700

    八 初稿

---
+++ 投资回报循环.java
@@ -0,0 +1,17 @@
+class 投资回报循环 {
+  public static void main (String[] 参数) {
+    float 账户余额 = 1000f;
+    float 回报率 = 0.08f;
+    int 年限 = 20;
+    int 年份 = 0;
+    
+    while (年份 < 年限) {
+      if (账户余额 > 2000) {
+        break;
+      }
+      年份 = 年份 + 1;
+      账户余额 = (1 + 回报率) * 账户余额;
+    }
+    System.out.println(年份 + "年后变成" + 账户余额 + "元");
+  }
+}
