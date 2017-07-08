432f8b6672eef1fb6ae4a2f1372d621aa1c62a8d
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-07-07 22:25:25 -0700

    七 初稿

---
+++ 循环死.java
@@ -0,0 +1,5 @@
+class 循环死 {
+  public static void main (String[] 参数) {
+    main(参数);
+  }
+}
---
+++ 条件语句.java
@@ -0,0 +1,12 @@
+class 条件语句 {
+  public static void main (String[] 参数) {
+    int 年龄 = 21;
+    if (年龄 < 5) {
+      System.out.println("这是哪家闺女啊?爸妈在哪儿呢?");
+    } else if (年龄 < 20) {
+      System.out.println("没到法定婚龄! 等几年再结婚吧");
+    } else {
+      System.out.println("妹妹,真想嫁也拦不住你.要不再考虑一天?");
+    }
+  }
+}
